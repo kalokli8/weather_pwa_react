5db86be0bf36070b6915d50d608b7e2be973d660
kalokli8 <42929320+kalokli8@users.noreply.github.com>
2022-03-31 17:47:29 +0800

    Update fetchWeather.js

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const URL = "https://api.openweathermap.org/data/2.5/weather";
+// example API KEY for testing
 const API_KEY = "f33a484cf794d08d0148764789aaba32";
 
 export const fetchWeather = async (query) => {
